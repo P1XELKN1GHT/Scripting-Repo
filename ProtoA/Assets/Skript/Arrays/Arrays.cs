f8320bed9a6c4974b11ec289c6a9e2e88e17d6dc
Spencer <10868468@uvu.edu>
2022-02-18 12:42:19 -0700

    Added 10 arrays into the new Arrays script

---
+++ ProtoA/Assets/Skript/Arrays/Arrays.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{/*
+    public string[] friendsnames = { "Anne", "Jerry", "Ron", "Adam", "April", "Andy", "Donna" };
+    //       Place holder values   { 0,   1,     2,    3,    4,     5,   6,}
+    public int[] numbahs = { 23, 46, 58, 19, 203, 64, 32 };
+    public string[] characters = { "sokka", "aang", "katara", "toph", "appa", "zuko", "momo" };
+    public int[] fineadditionstothecollection = { 1, 2, 3, 4 };
+    public string[] GhibliMovies = { "Laputa", "mononoke", "Spirited Away", "Kiki's Delivery service" };
+    public int[] Numeros = { 34, 23, 12, 45, 56, 67 };
+    public string[] Droids = { "C-3P0", "R2-D2", "R4", "Probe Droid", "K-2S0" };
+    public string[] Colors = { "red", "orange", "Blue", "green", "yellow", "purple" };
+    public int[] Numberofmodels = { 5, 10, 13, 15, 1, 20 };
+    public int[] Sets = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+    */
+    // Start is called before the first frame update
+    void Start()
+    {/*
+        Debug.Log(friendsnames[2] + " Knows more than you");
+        Debug.Log("former friend" + friendsnames[0]);
+        friendsnames[0] = " Jean-Ralphio";
+        Debug.Log("New friend" + friendsnames[0]);
+        
+
+        foreach(int numbahs in numbahs)
+        {
+            Debug.Log(numbahs);
+        }
+        foreach(string friendsnames in friendsnames)
+        {
+            print(friendsnames);
+        }
+        foreach(int fineadditionstothecollection in fineadditionstothecollection)
+        {
+            Debug.Log(fineadditionstothecollection);
+        }
+        foreach(string characters in characters)
+        {
+            print(characters);
+        }
+        foreach(int Sets in Sets)
+        {
+            Debug.Log(Sets);
+        }
+        foreach(string Droids in Droids)
+        {
+            print(Droids);
+        }
+        foreach(int Numeros in Numeros)
+        {
+            Debug.Log(Numeros);
+        }
+        foreach(string GhibliMovies in GhibliMovies)
+        {
+            print(GhibliMovies);
+        }
+        foreach(int Numberofmodels in Numberofmodels)
+        {
+            Debug.Log(Numberofmodels);
+        }
+        foreach(string Colors in Colors)
+        {
+            print(Colors);
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- ProtoA/Assets/Skript/Balloonie.cs
+++ ProtoA/Assets/Skript/Balloonie.cs
@@ -7,7 +7,7 @@ public class Balloonie : MonoBehaviour
 
     public int givescore = 100; //score for each balloon popped
 
-    public int clicks2pop = 3; //clicks it takes to pop each balloon
+    public int clicks2pop = 5; //clicks it takes to pop each balloon
 
     public float scaleup = 0.10f; // scaa;e increase for each click on a balloon
     // Start is called before the first frame update
@@ -20,8 +20,18 @@ public class Balloonie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
+    }
+    void OnMouseDown()
+    {
         clicks2pop -= 1; //reduce clicks by one
         transform.localScale += Vector3.one * scaleup; // (.one is used to increas x,y, and z) used to increase the size of the balloons
 
-           }
+        if(clicks2pop == 0)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
 }
