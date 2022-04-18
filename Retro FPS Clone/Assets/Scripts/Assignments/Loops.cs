0c3cfe941b7769170bc6a7e5edbf21ba4c4577dd
Spencer <10868468@uvu.edu>
2022-04-17 21:02:48 -0600

    Finished the loops script

--- Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
+++ Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
@@ -5,27 +5,73 @@ using UnityEngine;
 public class Loops : MonoBehaviour
 {
 
-    public int numEwoks = 20;
+    /*public int numEwoks = 20;
     public int ducks = 50;
+    public int Legos = 99;
+    public int RainDrops = 38;
+    public int numCrackers = 12;
+    public int bottles = 23;
+    public int candies = 87;
+    public int shoePairs = 43;
+    public int numCups = 5;
+    public int rooms = 16;
     
     // Start is called before the first frame update
     void Start()
     {
-        /*for(int i = 0; i <= numEwoks; i++)// will have a finate amount of times it loops, i++ is short hand for i+1, can also be used as i--
+        for(int i = 0; i <= numEwoks; i++)// will have a finate amount of times it loops, i++ is short hand for i+1, can also be used as i--
         {
             Debug.Log("There are" + i + "Ewoks on a log!");
-        }*/
+        }
+        for(int i = 99; i >= Legos; i--)
+        {
+            Debug.Log("There are" + i + "Lego bricks!");
+        }
+        for(int i = 0; i <= numCrackers; i++)
+        {
+            Debug.Log("There are" + i + "Crackers!");
+        }
+        for(int i = 87; i >= candies; i--) 
+            Debug.Log("There are" + i + "Candies!");
+        }
+        for(int i = 0; i <= numCups; i++)
+        {
+            Debug.Log("There are" + i + "Cups!");
+        }
+
+
+
+        
         while(ducks >0) // The same is true for counting up, but using <0 rather than >0. [Make sure the Inspector isnt overinding the code.]
         {
             print(ducks);
             print("All the ducks have flown south for winter.");
             ducks --;
         }
+        while(RainDrops <38) 
+        {
+            print(RainDrops);
+            print("There are raindrops falling from the sky ");
+            RainDrops ++;
+        }
+        while(bottles >0) 
+        {
+            print(bottles);
+            print("All the bottles have broken.");
+            bottles --;
+        }
+        while(shoePairs <43) 
+        {
+            print(shoePairs);
+            print("The Shoes keep building.");
+            shoePairs ++;
+        }
+        while(rooms >0)
+        {
+            print(rooms);
+            print("There are less and less rooms in each house.");
+            rooms --;
+        }*/
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+   
--- Retro FPS Clone/Assets/Scripts/FPS_Controller.cs
+++ Retro FPS Clone/Assets/Scripts/FPS_Controller.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class FPS_Controller : MonoBehaviour
 {
+    public static FPS_Controller instance;
+
     [Header("Controller")]
     public float moveSpeed;
     public float mouseSensitivity = 1.0f;
     private Rigidbody rb;
     private Vector3 moveInput;
     private Vector2 mouseInput;
+
+    public Camera viewCam;
+
+    [Header("Ammo")]
+    public GameObject bulletImpact;
+    public int currentAmmo;
+
+    private void Awake()
+    {
+        instance = this;
+    }
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         //Hide and Lock cursor
-        Cursor.visible = false;
+        Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -24,11 +37,33 @@ public class FPS_Controller : MonoBehaviour
     void Update()
     {
         moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Vector3 moveH = transform.right * moveInput.x;
-        Vector3 moveV = transform.forward * moveInput.z;
+        Vector3 moveH = transform.right * moveInput.x; // Blending movement for diagonal movement
+        Vector3 moveV = transform.forward * moveInput.z; // See above note
         rb.velocity = (moveH + moveV) * moveSpeed;
 
-        mouseInput = new Vector2(Input.GetAxis("Mouse x"), Input.GetAxis("Mouse Y")) * mouseSensitivity;
+        mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * mouseSensitivity;
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - mouseInput.x, transform.rotation.eulerAngles.z);
+        
+     if(Input.GetMouseButtonDown(0))
+        {
+         if(currentAmmo > 0)
+         {
+            Ray ray = viewCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                print("You hit" + hit.transform.name);
+                Instantiate(bulletImpact, hit.point, transform.rotation);
+            }
+            else
+            {
+                print("You Missed, didn't hit a thing");
+            }
+
+            currentAmmo --;
+         }
+    
+        }
     }
 }
